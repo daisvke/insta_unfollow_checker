dd19058fde2dc736607e54deeff2a8f0585eb893
dtanigaw <dtanigaw@student.42.fr>
2025-04-11 18:16:34 +0200

    Unfollowerslist: OK + Add outfile saving

---
+++ ascii_format.py
@@ -0,0 +1,42 @@
+# ANSI escape codes for text formatting
+
+RESET = "\033[0m"
+BOLD = "\033[1m"
+DIM = "\033[2m"
+
+# Colors
+BLACK = "\033[30m"
+RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+BLUE = "\033[34m"
+MAGENTA = "\033[35m"
+CYAN = "\033[36m"
+WHITE = "\033[37m"
+
+# Bright colors
+BRIGHT_BLACK = "\033[90m"
+BRIGHT_RED = "\033[91m"
+BRIGHT_GREEN = "\033[92m"
+BRIGHT_YELLOW = "\033[93m"
+BRIGHT_BLUE = "\033[94m"
+BRIGHT_MAGENTA = "\033[95m"
+BRIGHT_CYAN = "\033[96m"
+BRIGHT_WHITE = "\033[97m"
+
+# Background colors
+BG_BLACK = "\033[40m"
+BG_RED = "\033[41m"
+BG_GREEN = "\033[42m"
+BG_YELLOW = "\033[43m"
+BG_BLUE = "\033[44m"
+BG_MAGENTA = "\033[45m"
+BG_CYAN = "\033[46m"
+BG_WHITE = "\033[47m"
+
+# Logs levels
+INFO = f"{YELLOW}[INFO]{RESET}"
+WARNING = f"{MAGENTA}[WARNING]{RESET}"
+ERROR = f"{RED}[ERROR]{RESET}"
+DONE = f"{GREEN}[DONE]{RESET}"
+FOUND = f"{GREEN}[FOUND]{RESET}"
--- checker.py
+++ checker.py
@@ -1,13 +1,19 @@
+#!/usr/bin/env python3
+
 from sys import stderr
 import zipfile
 import os
 from argparse import ArgumentParser
+from ascii_format import DONE, ERROR, YELLOW, RESET, INFO
 
 extract_to_directory = "tmp/"
-path_to_lists_folder = extract_to_directory + "connections/followers_and_following/"
+save_to_file = "unfollowers.txt"
+path_to_lists_folder = \
+    extract_to_directory + "connections/followers_and_following/"
 path_to_following_folder = path_to_lists_folder + "following.html"
 path_to_followers_folder = path_to_lists_folder + "followers_1.html"
 username_prefix = "instagram.com/"
+
 
 class InstaUnfollowChecker:
 
@@ -16,19 +22,20 @@ class InstaUnfollowChecker:
         filepath: str,
         search_name: str,
         case_insensitive: bool,
-        verbose: bool 
-        ):
+        verbose: bool,
+        save: bool
+            ):
         self.filepath = filepath
         self.search_name = search_name
         self.case_insensitive = case_insensitive
         self.verbose = verbose
+        self.save = save
 
-        self.following = []
-        self.followers = []
+        self.following: list[str] = []
+        self.followers: list[str] = []
 
     def unzip(self) -> None:
-        # Specify the path to the ZIP file and the directory to extract to
-
+        """Unzip the ZIP file and extract its content in the given directory"""
         # Create the directory if it doesn't exist
         os.makedirs(extract_to_directory, exist_ok=True)
 
@@ -37,9 +44,11 @@ class InstaUnfollowChecker:
             # Extract all the contents into the specified directory
             zip_ref.extractall(extract_to_directory)
 
-        print(f'Extracted all files to {extract_to_directory}')
+        if self.verbose:
+            print(f"{DONE} Extracted all files to {extract_to_directory}\n")
 
     def parse_list(self, listpath: str) -> list[str]:
+        """Parse the ZIP file content to get a list of usernames"""
         results = []
 
         with open(listpath, 'r') as list:
@@ -54,24 +63,75 @@ class InstaUnfollowChecker:
             if quote_index != -1:  # If '"' is found
                 # Extract the substring from the start to the index of '"'
                 extracted_string = part[:quote_index]
-                results.append(extracted_string)
+                if '<' not in extracted_string:
+                    results.append(extracted_string)
 
-        print(results)
+        return results
 
+    def get_unfollowers(
+        self, following: list[str], followers: list[str]
+            ) -> tuple[list[str], int]:
+        """
+        Get the unfollowers' list by comparing the followers'
+        and the following's lists
+        """
+
+        unfollowers = []
+        count = 0
+
+        for username in following:
+            if username not in followers:
+                unfollowers.append(username)
+                count += 1
+        return unfollowers, count
+
+    def print_results(self, unfollowers: list[str], count: int) -> None:
+        """Print the results."""
+        # If the mode is on, open the file in which we will save the resuts
+        if self.save:
+            file = open(save_to_file, 'w')
+
+        # Print each username and write it on the outfile if the mode is on
+        for username in unfollowers:
+            if self.save:
+                file.write(f"{username}\n")
+            print(username)
+
+        # Print some useful information at the end
+        if self.verbose:
+            print(f"\n{INFO} Total: {YELLOW}{count}{RESET} unfollowers.")
+            if self.save:
+                print(f"{INFO} Saved results in {YELLOW}{save_to_file}{RESET}")
 
     def run(self) -> None:
-        try:
-            self.unzip()
-            self.parse_list(path_to_following_folder)
-        except Exception as e:
-            print(f"Error: {e}\n", file=stderr)
+        """Run the checker"""
 
+        following: list[str] = []
+        followers: list[str] = []
+        unfollowers: list[str] = []
+
+        try:
+            # Unzip the ZIP file to get its content
+            self.unzip()
+
+            # Get the following usernames
+            following = self.parse_list(path_to_following_folder)
+            # Get the followers' usernames
+            followers = self.parse_list(path_to_followers_folder)
+
+            # Get the unfollowers' list and print the results
+            unfollowers, count = self.get_unfollowers(following, followers)
+            self.print_results(unfollowers, count)
+
+        except Exception as e:
+            print(f"{ERROR} {e}\n", file=stderr)
 
 
 def parse_args():
     """Set up argparse and return the given arguments."""
     parser = ArgumentParser(
-        description="Return a list of usernames that are not following you back.."
+        description="Return a list of usernames that are not"
+                    "following you back."
     )
     parser.add_argument(
         'zipfile', type=str, help='the zip file to process'
@@ -79,7 +139,12 @@ def parse_args():
     parser.add_argument(
         '-v', '--verbose', action='store_true', help="Enable verbose mode.")
     parser.add_argument(
-        '-s', '--search-string', type=str, help='enable single username search')
+        '-o', '--outfile', action='store_true',
+        help="Save the unfollowers' list in a file"
+    )
+    parser.add_argument(
+        '-s', '--search-username', type=str,
+        help='enable single username search')
     parser.add_argument(
         '-i', '--case-insensitive', action='store_true',
         help='Enable case-insensitive mode'
@@ -95,15 +160,17 @@ def parse_args():
 
     return parser.parse_args()
 
+
 if __name__ == "__main__":
     # Parse command-line arguments
     args = parse_args()
 
     checker = InstaUnfollowChecker(
             args.zipfile,
-            args.search_string,
+            args.search_username,
             args.case_insensitive,
-            args.verbose
+            args.verbose,
+            args.outfile
             )
 
     checker.run()
